178b7838ab47ad1457a6a8ac805845d9675c5506
DrOctoPunk <sales@octopunk.io>
2026-02-20 13:54:52 -0500

    Fix App Service template module order

--- azure/app-service-sql/main.tf
+++ azure/app-service-sql/main.tf
@@ -35,6 +35,18 @@ resource "azurerm_resource_group" "main" {
   location = var.azure_region
 }
 
+module "sql_database" {
+  source = "./modules/sql-database"
+
+  resource_group_name       = azurerm_resource_group.main.name
+  location                  = azurerm_resource_group.main.location
+  project_name              = var.project_name
+  environment               = var.environment
+  sql_server_admin_password = var.sql_server_admin_password
+  database_sku              = var.database_sku
+  max_size_gb               = var.database_max_size_gb
+}
+
 module "app_service" {
   source = "./modules/app-service"
 
@@ -43,25 +55,10 @@ module "app_service" {
   project_name        = var.project_name
   environment         = var.environment
 
-  app_service_plan_sku = var.app_service_plan_sku
-  docker_image         = var.docker_image
-  docker_tag           = var.docker_tag
-  app_port             = var.app_port
-  health_check_path    = var.health_check_path
-
-  sql_server_admin_password = var.sql_server_admin_password
+  app_service_plan_sku       = var.app_service_plan_sku
+  docker_image               = var.docker_image
+  docker_tag                 = var.docker_tag
+  app_port                   = var.app_port
+  health_check_path          = var.health_check_path
   database_connection_string = module.sql_database.connection_string
-}
-
-module "sql_database" {
-  source = "./modules/sql-database"
-
-  resource_group_name = azurerm_resource_group.main.name
-  location            = azurerm_resource_group.main.location
-  project_name        = var.project_name
-  environment         = var.environment
-
-  sql_server_admin_password = var.sql_server_admin_password
-  database_sku              = var.database_sku
-  max_size_gb               = var.database_max_size_gb
 }
